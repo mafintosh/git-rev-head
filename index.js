7bca730fece72c81a0b8088569e614bc53fa65cd
Mathias Buus <mathiasbuus@gmail.com>
2020-11-27 14:20:35 +0100

    fix cb

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 const path = require('path')
 
-module.exports = function (repo, cb) {
+module.exports = function (repo) {
   return new Promise((resolve, reject) => {
     fs.readFile(path.join(repo, '.git/HEAD'), 'utf-8', function (err, head) {
       if (err) return reject(err)
-      if (/^[a-f0-9]+$/.test(head.trim())) return cb(null, head.trim())
+      if (/^[a-f0-9]+$/.test(head.trim())) return resolve(head.trim())
       const ref = head.trim().split('ref: ')[1]
       if (!ref) return reject(new Error('Could not parse head: ' + head.trim()))
       fs.readFile(path.join(repo, '.git', ref), 'utf-8', function (err, hash) {
