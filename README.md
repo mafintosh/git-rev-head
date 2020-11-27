8cf22117c466854f0d281392c71f2a17c1eccc88
Mathias Buus <mathiasbuus@gmail.com>
2020-11-27 14:19:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# git-head-hash
+
+Get the current HEAD hash of a Git repo
+
+```
+npm install git-head-hash
+```
+
+## Usage
+
+``` js
+const gitHeadHash = require('git-head-hash')
+
+console.log(await gitHeadHash('./repo')) // prints deadbeef...
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+
+module.exports = function (repo, cb) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path.join(repo, '.git/HEAD'), 'utf-8', function (err, head) {
+      if (err) return reject(err)
+      if (/^[a-f0-9]+$/.test(head.trim())) return cb(null, head.trim())
+      const ref = head.trim().split('ref: ')[1]
+      if (!ref) return reject(new Error('Could not parse head: ' + head.trim()))
+      fs.readFile(path.join(repo, '.git', ref), 'utf-8', function (err, hash) {
+        if (err) return resolve(null)
+        resolve(hash.trim())
+      })
+    })
+  })
+}
+
+module.exports('.').then(console.log)
